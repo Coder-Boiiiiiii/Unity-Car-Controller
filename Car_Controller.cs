5287da9f6ff06cc1730f7a86a85cf7cb60acb278
Coder-Boiiiiiii <78927830+Coder-Boiiiiiii@users.noreply.github.com>
2021-10-22 21:50:01 +0500

    Update Car_Controller.cs

--- Car_Controller.cs
+++ Car_Controller.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using TMPro;
 
 public class Car_Controller : MonoBehaviour
 {
-    //Variables
+    //Public Variables
     [Header("Wheel Colliders")]
     public WheelCollider FL;
     public WheelCollider FR;
@@ -30,6 +31,15 @@ public class Car_Controller : MonoBehaviour
     public float Max_Steer_Angle = 20f;
     public float  BrakeForce = 150f;
 
+    [Space(3)]
+
+    //These are the speeds for each gear
+    //The Brake and Reverse gears appear automatically so don't worry about those
+    //The Speeds MUST be in kph
+    public List<int> Gears_Speed;
+
+    [Space(3)]
+
     public float handBrakeFrictionMultiplier = 2;
     private float handBrakeFriction  = 0.05f;
     public float tempo;
@@ -52,21 +62,66 @@ public class Car_Controller : MonoBehaviour
     public float Sideways_Extremium_Value_When_Drifting;
 
     [Header("Light Setting(s)")]
-    public bool Enable_Headlights;
-    public bool Enable_Brakelights;
-    public bool Enable_Reverselights;
+
+    [Header("Lights (With Light Settings)")]
+    public bool Enable_Headlights_Lights;
+    public bool Enable_Brakelights_Lights;
+    public bool Enable_Reverselights_Lights;
 
     public Light[] HeadLights;
     public Light[] BrakeLights;
     public Light[] ReverseLights;
 
+    [Space(4)]
+
+    [Header("Light (With MeshRenderer")]
+    public bool Enable_Headlights_MeshRenderers;
+    public bool Enable_Brakelights_MeshRenderers;
+    public bool Enable_Reverselights_MeshRenderers;
+
+    public MeshRenderer[] HeadLights_MeshRenderers;
+    public MeshRenderer[] BrakeLights_MeshRenderers;
+    public MeshRenderer[] ReverseLights_MeshRenderers;
+
     [Header("Particle System(s) Settings")]
     public bool Use_Particle_Systems;
     public ParticleSystem[] Car_Smoke_From_Silencer;//Sorry, couldn't think of a better name :P
 
+    [Header("UI Settings")]
+    public bool Use_TMP;
+    public bool Use_Default_UI;
+
+    [Space(3)]
+
+    public bool Show_Speed_In_KPH;
+    public Text Speed_Text_UI;
+    public TextMeshProUGUI Speed_Text_TMPPro;
+
+    [Space(3)]
+
+    public Text Gear_Text;
+    public TextMeshProUGUI Gear_TMPro;
+
     [Header("Other Settings")]
     public Transform Center_of_Mass;
     public  float frictionMultiplier = 3f;
+    public Rigidbody Car_Rigidbody;
+
+    [Header("Debug")] //These are variables that are read only so dont chnage them, they are only there if u wanna use them for UI like speed or RPM;
+    public float RPM_FL;
+    public float RPM_FR;
+    public float RPM_BL;
+    public float RPM_BR;
+
+    [Space(8)]
+
+    public float Car_Speed_KPH;
+    public float Car_Speed_MPH;
+
+    [Space(4)]
+
+    public string Current_Gear;
+    public int Current_Gear_num;
 
     //private Variables
     private Rigidbody rb;
@@ -75,6 +130,10 @@ public class Car_Controller : MonoBehaviour
     private WheelFrictionCurve  FRforwardFriction, FRsidewaysFriction;
     private WheelFrictionCurve  BLforwardFriction, BLsidewaysFriction;
     private WheelFrictionCurve  BRforwardFriction, BRsidewaysFriction;
+
+    //Debug Values in Int Form
+    int Car_Speed_In_KPH;
+    int Car_Speed_In_MPH;
 
     //Private Audio Variables
     private float Forward_volume;
@@ -97,26 +156,77 @@ public class Car_Controller : MonoBehaviour
             }
         }
 
-        if(Enable_Headlights){
+        //Set the current gear to 0
+        Current_Gear = "0";
+        Current_Gear_num = 0;
+        
+        //Here we just set the lights to turn on and off at play.
+
+        //We turn the headlights on here
+        if(Enable_Headlights_Lights){
             foreach(Light H in HeadLights){
                 H.enabled = true;
             }
         }
 
-        if(Enable_Reverselights){
+        if(Enable_Headlights_MeshRenderers){
+            foreach(MeshRenderer HM in HeadLights_MeshRenderers){
+                HM.enabled = true;
+            }
+        }
+
+        //Here we turn the reverse light(s) off
+        if(Enable_Reverselights_Lights){
             foreach(Light R in ReverseLights){
                 R.enabled = false;
             }
         }
 
-        if(Enable_Brakelights){
+        if(Enable_Reverselights_MeshRenderers){
+            foreach(MeshRenderer RM in ReverseLights_MeshRenderers){
+                RM.enabled = false;
+            }
+        }
+
+        //Here we turn off the brakelights
+        if(Enable_Brakelights_Lights){
             foreach(Light B in BrakeLights){
                 B.enabled = false;
+            }
+        }
+
+        if(Enable_Brakelights_MeshRenderers){
+            foreach(MeshRenderer BM in BrakeLights_MeshRenderers){
+                BM.enabled = true;
             }
         }
     }
 
     public void FixedUpdate(){
+        //Changing Gears
+        if(Gears_Speed[Current_Gear_num] < Car_Speed_KPH && Current_Gear_num != Gears_Speed.Count){
+            Current_Gear_num++;
+            Current_Gear = (Current_Gear_num + 1).ToString();
+        }
+
+        if(Gears_Speed[Current_Gear_num] > Car_Speed_KPH && Current_Gear_num != 0){
+            Current_Gear_num--;
+            Current_Gear = (Current_Gear_num + 1).ToString();
+        }
+
+        if(Car_Speed_In_KPH == 0){
+            Current_Gear = "0";
+        }
+
+        //Setting the gear text to the current gear
+        if(Use_TMP){
+            Gear_TMPro.SetText(Current_Gear);
+        }
+
+        if(Use_Default_UI){
+            Gear_Text.text = Current_Gear;
+        }
+
         //Making The Car Move Forward or Backward
         BL.motorTorque = Input.GetAxis("Vertical") * Motor_Torque;
         BR.motorTorque = Input.GetAxis("Vertical") * Motor_Torque;
@@ -124,6 +234,40 @@ public class Car_Controller : MonoBehaviour
         //Making The Car Turn
         FL.steerAngle = Input.GetAxis("Horizontal") * Max_Steer_Angle;
         FR.steerAngle = Input.GetAxis("Horizontal") * Max_Steer_Angle;
+
+        //Showing the RPM for the wheels
+        RPM_FL = FL.rpm;
+        RPM_BL = BL.rpm;
+        RPM_FR = FR.rpm;
+        RPM_BR = BR.rpm;
+
+        //Changing speed of the car
+        Car_Speed_KPH = Car_Rigidbody.velocity.magnitude * 3.6f;
+        Car_Speed_MPH = Car_Rigidbody.velocity.magnitude * 2.237f;
+
+        Car_Speed_In_KPH = (int) Car_Speed_KPH;
+        Car_Speed_In_MPH = (int) Car_Speed_MPH;
+
+        //Showing Car Speed
+        if(Use_Default_UI){
+            if(Show_Speed_In_KPH){
+                Speed_Text_UI.text = Car_Speed_In_KPH.ToString();
+            }
+
+            if(!Show_Speed_In_KPH){
+                Speed_Text_UI.text = Car_Speed_In_MPH.ToString();
+            }
+        }
+
+        if(Use_TMP){
+            if(Show_Speed_In_KPH){
+                Speed_Text_TMPPro.SetText(Car_Speed_In_KPH.ToString());
+            }
+
+            if(!Show_Speed_In_KPH){
+                Speed_Text_TMPPro.SetText(Car_Speed_In_MPH.ToString());
+            }
+        }
 
         //Make Car Boost
         if(Input.GetKey(KeyCode.LeftShift)){
@@ -149,10 +293,14 @@ public class Car_Controller : MonoBehaviour
 
         if(wheelHit1.sidewaysSlip < 0 )	
             tempo = (1 + -Input.GetAxis("Horizontal")) * Mathf.Abs(wheelHit1.sidewaysSlip *handBrakeFrictionMultiplier);
+
             if(tempo < 0.5) tempo = 0.5f;
+
         if(wheelHit1.sidewaysSlip > 0 )	
             tempo = (1 + Input.GetAxis("Horizontal") )* Mathf.Abs(wheelHit1.sidewaysSlip *handBrakeFrictionMultiplier);
+
             if(tempo < 0.5) tempo = 0.5f;
+
         if(wheelHit1.sidewaysSlip > .99f || wheelHit1.sidewaysSlip < -.99f){
             //handBrakeFriction = tempo * 3;
             float velocity = 0;
@@ -160,11 +308,15 @@ public class Car_Controller : MonoBehaviour
             }
 
         if(wheelHit2.sidewaysSlip < 0 )	
-            tempo = (1 + -Input.GetAxis("Horizontal")) * Mathf.Abs(wheelHit2.sidewaysSlip *handBrakeFrictionMultiplier) ;
+            tempo = (1 + -Input.GetAxis("Horizontal")) * Mathf.Abs(wheelHit2.sidewaysSlip *handBrakeFrictionMultiplier);
+
             if(tempo < 0.5) tempo = 0.5f;
+        
         if(wheelHit2.sidewaysSlip > 0 )	
             tempo = (1 + Input.GetAxis("Horizontal") )* Mathf.Abs(wheelHit2.sidewaysSlip *handBrakeFrictionMultiplier);
+        
             if(tempo < 0.5) tempo = 0.5f;
+        
         if(wheelHit2.sidewaysSlip > .99f || wheelHit2.sidewaysSlip < -.99f){
             //handBrakeFriction = tempo * 3;
             float velocity = 0;
@@ -173,10 +325,14 @@ public class Car_Controller : MonoBehaviour
 
         if(wheelHit3.sidewaysSlip < 0 )	
             tempo = (1 + -Input.GetAxis("Horizontal")) * Mathf.Abs(wheelHit3.sidewaysSlip *handBrakeFrictionMultiplier) ;
+        
             if(tempo < 0.5) tempo = 0.5f;
+        
         if(wheelHit3.sidewaysSlip > 0 )	
             tempo = (1 + Input.GetAxis("Horizontal") )* Mathf.Abs(wheelHit3.sidewaysSlip *handBrakeFrictionMultiplier);
+        
             if(tempo < 0.5) tempo = 0.5f;
+        
         if(wheelHit3.sidewaysSlip > .99f || wheelHit3.sidewaysSlip < -.99f){
             //handBrakeFriction = tempo * 3;
             float velocity = 0;
@@ -185,10 +341,14 @@ public class Car_Controller : MonoBehaviour
 
         if(wheelHit4.sidewaysSlip < 0 )	
             tempo = (1 + -Input.GetAxis("Horizontal")) * Mathf.Abs(wheelHit4.sidewaysSlip *handBrakeFrictionMultiplier) ;
+        
             if(tempo < 0.5) tempo = 0.5f;
+        
         if(wheelHit4.sidewaysSlip > 0 )	
             tempo = (1 + Input.GetAxis("Horizontal") )* Mathf.Abs(wheelHit4.sidewaysSlip *handBrakeFrictionMultiplier);
+        
             if(tempo < 0.5) tempo = 0.5f;
+        
         if(wheelHit4.sidewaysSlip > .99f || wheelHit4.sidewaysSlip < -.99f){
             //handBrakeFriction = tempo * 3;
             float velocity = 0;
@@ -199,19 +359,34 @@ public class Car_Controller : MonoBehaviour
             handBrakeFriction = tempo;
         }
 
-        //Enable reverse lights when car is reversing
         if(Input.GetKey(KeyCode.S)){
-            if(Enable_Reverselights){
+            //Change gear to "R"
+            Current_Gear = "R";
+            
+            //Enable reverse lights when car is reversing
+            if(Enable_Reverselights_Lights){
                 foreach(Light RL in ReverseLights){
                     RL.enabled = true;
+                }
+            }
+
+            if(Enable_Reverselights_MeshRenderers){
+                foreach(MeshRenderer RM in ReverseLights_MeshRenderers){
+                    RM.enabled = true;
                 }
             }
         }
 
         if(!Input.GetKey(KeyCode.S)){
-            if(Enable_Reverselights){
+            if(Enable_Reverselights_Lights){
                 foreach(Light Rl in ReverseLights){
                     Rl.enabled = false;
+                }
+            }
+
+            if(Enable_Reverselights_MeshRenderers){
+                foreach(MeshRenderer RM in ReverseLights_MeshRenderers){
+                    RM.enabled = false;
                 }
             }
         }
@@ -243,10 +418,19 @@ public class Car_Controller : MonoBehaviour
         if(Input.GetKey(KeyCode.Space) == true){
             Brakes = BrakeForce;
 
+            //Set the Current Gear to B
+            Current_Gear = "B";
+
             //Turn on brake lights
-            if(Enable_Brakelights){
+            if(Enable_Brakelights_Lights){
                 foreach(Light L in BrakeLights){
                     L.enabled = true;
+                }
+            }
+
+            if(Enable_Brakelights_MeshRenderers){
+                foreach(MeshRenderer BM in BrakeLights_MeshRenderers){
+                    BM.enabled = true;
                 }
             }
 
@@ -317,9 +501,15 @@ public class Car_Controller : MonoBehaviour
 
         if(!Input.GetKey(KeyCode.Space)){
             //Turn off brake lights
-            if(Enable_Brakelights){
+            if(Enable_Brakelights_Lights){
                 foreach(Light L in BrakeLights){
                     L.enabled = false;
+                }
+            }
+
+            if(Enable_Brakelights_MeshRenderers){
+                foreach(MeshRenderer BM in BrakeLights_MeshRenderers){
+                    BM.enabled = false;
                 }
             }
         }
